031e0910d8516d1368ef81a7b1a183da708f1ab5
luka <luka@devvm.0.2.15>
2018-03-18 18:21:57 +0100

    Input works

--- hello.adb
+++ hello.adb
@@ -1,5 +1,5 @@
-with Ada.Text_IO; 
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure Hello is
 type Integer_Array is array(Positive range<>) of Integer;
@@ -13,17 +13,9 @@ begin
     end loop;
     return Elements;
 end Get_Input_Array;
-
-
-N : Integer;
+Input_Elements : Integer_Array := Get_Input_Array;
 begin
-    N := Integer'Value(Get_Line);
-    declare
-    Elements : Integer_array(1..N);
-    begin
-        for I in 1..N loop
-            Elements(I) := Integer'Value(Get_Line);
-            --Put_Line("hi");
-        end loop;
-    end;
+    for I in 1..Input_Elements'Length loop
+        Put(Input_Elements(I)); New_Line;
+    end loop;
 end Hello;
