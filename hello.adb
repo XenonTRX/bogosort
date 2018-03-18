d677ed635b7f1420cda7c0444c90602491fcaa6a
luka <luka@devvm.0.2.15>
2018-03-18 22:58:04 +0100

    Added Is_Sorted function

--- hello.adb
+++ hello.adb
@@ -6,6 +6,7 @@ procedure Hello is
     type Integer_Array is array(Positive range<>) of Integer;
     package Random_Integer is new Ada.Numerics.Discrete_Random(Result_Subtype=>Integer);
     use Random_Integer;
+    
     function Get_Input_Array return Integer_Array is
         N : Integer := Integer'Value(Get_Line);
         Elements : Integer_Array(1..N);
@@ -15,10 +16,12 @@ procedure Hello is
         end loop;
         return Elements;
     end Get_Input_Array;
+    
     function Random_Integer_In_Range(Lower, Higher: Integer; G: Generator) return Integer is
     begin
         return (Random(G) mod (Higher-Lower + 1)) + Lower;
     end Random_Integer_In_Range;
+    
     function Knuth_Shuffle(Input_Array: Integer_Array; G: Generator) return Integer_Array is
         Output_Array : Integer_Array(1..Input_Array'Length);
         J : Integer;
@@ -32,6 +35,22 @@ procedure Hello is
         end loop;
         return Output_Array;
     end Knuth_Shuffle;
+    
+    function Is_Sorted(Input_Array: Integer_Array) return Boolean is
+        Previous_value : Integer := Integer'First;
+    begin
+        for I in Input_Array'Range loop
+            if Previous_Value > Input_Array(I) then
+                return False;
+            end if;
+            Previous_Value := Input_Array(I);
+        end loop;
+        return True;
+    end Is_Sorted;
+
+    --function Binary_Bogosort(Input_Array: Integer_Array; G: Generator) return Integer_Array is
+    
+    --end Binary_Bogosort;
     Input_Elements : Integer_Array := Get_Input_Array;
     Shuffled : Integer_Array (1..Input_Elements'Length);
     G: Generator;
